4f365c2f9f73925b8dffd8efdc1d532b589d16a6
JackChin-txt <uniamlajack@gmail.com>
2023-12-10 12:42:40 +0800

    第二版
    第二版本:將上方按鈕跟fundimental統一、新增點cable或是bell會下滑到指定區域的功能

--- tool_introduction/introduction.js
+++ tool_introduction/introduction.js
@@ -3,7 +3,27 @@ ex:
 
 document.getElementById("fitness_calculator").addEventListener("click",function(){//點擊button頁面會導到calculator_page.html
     window.location.href = "../fitness_calculator/calculator_page.html";
+    
 }); 
 
 */
 
+document.getElementById("fitness_calculator").addEventListener("click",function(){//點擊button頁面會導到calculator_page.html
+    window.location.href = "../fitness_calculator/calculator_page.html"});
+
+document.getElementById("home").addEventListener("click",function(){//點擊button頁面會導到calculator_page.html
+    window.location.href = "../main_page/main_page.html"});
+    
+document.getElementById("Tools").addEventListener("change", function() {//點擊button會將頁面下滑到指定區域
+    var selectedValue = document.getElementById("Tools").value;
+    var targetElement;
+    
+    if (selectedValue === "Cable") {
+        targetElement = document.getElementById("Cable_single");
+    } else if (selectedValue === "Bell") {
+        targetElement = document.getElementById("Bell_multi");
+    }
+    
+    if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }});
